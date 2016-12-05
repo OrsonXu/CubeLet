cd2852ea8ecf35090e8f68cc26bd4da83acefdcc
OrsonXu <orson.xu@foxmail.com>
2016-12-05 14:30:34 +0800

    Add Main scene, helped by Zheng

---
+++ Assets/SelfDevelopment/Script/UI/LoadToGame.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadToGame : MonoBehaviour {
+
+    public int LevelIndex;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void LoadToGameScene()
+    {
+        SceneManager.LoadScene(LevelIndex + 1);
+    }
+}
